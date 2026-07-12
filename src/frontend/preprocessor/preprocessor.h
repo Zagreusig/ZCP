ef4dd8140c41605a4ec68adc4709762c4e56bce8
Zagreus <zagreusig@gmail.com>
2026-07-12 07:55:32 -0700

    WIP

--- src/frontend/preprocessor/preprocessor.h
+++ src/frontend/preprocessor/preprocessor.h
@@ -1,6 +1,39 @@
 #ifndef PREPROCESSOR_H
 #define PREPROCESSOR_H
 
+/**
+* include import with
+* # .. \\
+* define(def) undef ifndef
+* pragma once
+* if elif else fi
+* error warning
+* # turns macro to string
+* ## concats macro toks
+* <> "" for files
+* <> system, "" use current dir
+*/
 
+/**
+* Lexer -> preprocessor -> parser ...
+* Parse all of the headers for definitions, create a sort of global table of definitions
+* unordered map?
+*
+*
+* class Preprocessor
+* #define BIT 1
+* #define BANG(x) !x
+* SYMBOLS = {
+*  { "this header": { list, of, definitions, here, lol }},
+*  { "preproc.h": { {"Preprocessor", PREPROC::CLASS, false}, {"BIT", PREPROC::MACRO, false}, { "BANG", PREPROC::MACRO, true}}}
+* }
+*/
+class Preprocessor {
+public:
+   Preprocessor() {}
+
+private:
+
+};
 
 #endif // PREPROCESSOR_H
